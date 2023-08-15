2ec04cb04fefbaf332c4b134a23c7a2c5554822e
Mike Audi <mike@mytiki.com>
2023-08-14 23:31:25 -0500

    fix: add ts docs (ai generated) for blink receipt obj.

--- src/shipment.ts
+++ src/shipment.ts
@@ -5,7 +5,17 @@
 
 import type { Product } from './product';
 
+/**
+ * Represents a shipment with its status and a list of products included in the shipment.
+ */
 export interface Shipment {
+  /**
+   * The status of the shipment.
+   */
   status: string;
+
+  /**
+   * An array of products included in the shipment.
+   */
   products: Product[];
 }
