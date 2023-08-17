3d2b6a17b8761d52270eb44a8fd587e6376635cc
Mike Audi <mike@mytiki.com>
2023-08-16 19:42:01 -0500

    fix: add stock example app

---
+++ example/src/main.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+import './assets/main.css';
+import { createApp } from 'vue';
+
+import App from '@/app.vue';
+
+createApp(App).mount('#app');
