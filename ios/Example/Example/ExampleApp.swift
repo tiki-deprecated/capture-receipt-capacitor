2cdf88d94462c721dcfca7de99edd582f719cfea
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2023-08-17 17:14:03 -0300

    [iOS] add Example and fix Retailer
    Add Example project to run and test the functions and fix Retailer problems

---
+++ ios/Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Jesse Monteiro Ferreira on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- ios/Src/Mytiki/Sdk/Capture/Receipt/Capacitor/Retailer.swift
+++ ios/Src/Mytiki/Sdk/Capture/Receipt/Capacitor/Retailer.swift
@@ -8,57 +8,84 @@ import BlinkReceipt
 import BlinkEReceipt
 import Capacitor
 
-class Retailer {
+public class Retailer {
     
-    func initialize(req: ReqInitialize){
+    public init(req: ReqInitialize) {
         BRScanManager.shared().licenseKey = req.licenseKey
         BRScanManager.shared().prodIntelKey = req.productKey
         BRAccountLinkingManager.shared()
     }
-    
-    func account (retailer: BRAccountLinkingRetailer, username: String, password: String, dayCutoff: Int) {
-        let connection = BRAccountLinkingConnection(retailer: retailer, username: username, password: password)
+    public func account (retailer: RetailerCommon, username: String, password: String, dayCutoff: Int) {
+        let connection = BRAccountLinkingConnection(retailer: retailer.toBRAccountLinkingRetailer()!, username: username, password: password)
         connection.configuration.dayCutoff = dayCutoff
         connection.configuration.returnLatestOrdersOnly = false
-        connection.configuration.countryCode = "US"
-        
+//        connection.configuration.countryCode = "US"
         let error = BRAccountLinkingManager.shared().linkRetailer(with: connection)
         if (error == .none) {
-          // Success
+            print("xyz passou do primeiro if sem erros")
+            verifyConnection(retailer: retailer, username: username, password: password)
         }
-        
-        let taskId = BRAccountLinkingManager.shared().verifyRetailer(with: connection, withCompletion: { error, viewController, sessionId in
 
-          if (error == .verificationNeeded) {
+    }
+    
+    public func verifyConnection(retailer: RetailerCommon, username: String, password: String) {
+        
+        let connection = BRAccountLinkingConnection(retailer: retailer.toBRAccountLinkingRetailer()!, username: username, password: password)
+        connection.configuration.dayCutoff = 30
+        let taskId = BRAccountLinkingManager.shared().verifyRetailer(with: connection, withCompletion: { errorCode, viewController, sessionId in
+
+          if (errorCode == .verificationNeeded) {
+              print("xyz Caiu no verification needed")
             // Display UIViewController for 2FA verification, CAPTCHA or other user input
 //              viewController.shared.window.first?.rootViewController
 
-          } else if (error == .verificationCompleted) {
+          } else if (errorCode == .verificationCompleted) {
             // Dismiss the previously presented UIViewController
 //              viewController!.dismiss(animated: true)
 
           } else {
 
-            if (error == .noCredentials) {
+            if (errorCode == .noCredentials) {
               // Error: Credentials have not been provided
-            } else if (error == .internal) {
+            } else if (errorCode == .internal) {
               // Error: Unexpected error
-            } else if (error == .parsingFail) {
+            } else if (errorCode == .parsingFail) {
               // Error: General Error
-            } else if (error == .invalidCredentials) {
+            } else if (errorCode == .invalidCredentials) {
               // Error: Probable cause of the failure is invalid credentials
-            } else if (error == .cancelled) {
+            } else if (errorCode == .cancelled) {
               // Operation has been cancelled.
               // Dismiss any previously presented user input UIViewController
             }
             // Verification Completed Successfully
             // Account can be linked
             BRAccountLinkingManager.shared().linkRetailer(with: connection)
+              
           }
         })
-
-
     }
+    
+    public func orders(){
+        let taskId = BRAccountLinkingManager.shared().grabNewOrders(for: .amazon) { retailer, order, remaining, viewController, errorCode, sessionID in
+
+          if (errorCode == .verificationNeeded) {
+            // Display UIViewController for 2FA verification, CAPTCHA or other user input
+          } else if errorCode == .invalidKey {
+              print("ALGO ERRADO NÃO ESTÁ CERTO")
+          }else if errorCode == .verificationCompleted {
+            // Dismiss the previously presented UIViewController
+          } else if errorCode == .none {
+            // Order may be returned here
+            if (remaining <= 0) {
+              // Grab Orders Completed Successfully
+              // No more orders to fetch
+            }
+          } else {
+            // Grab Orders Failed. Check error for more info
+          }
+        }
+    }
+    
     
 }
 //            how use the RetailerCommon
