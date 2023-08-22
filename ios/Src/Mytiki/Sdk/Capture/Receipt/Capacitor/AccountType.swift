2e2cb54d2fee6f8adce1778aa32fbb0254ea803d
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2023-08-22 09:40:38 -0300

    Create AccountType Enum

---
+++ ios/Src/Mytiki/Sdk/Capture/Receipt/Capacitor/AccountType.swift
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+import Foundation
+
+public enum AccountType {
+    let type: String
+    let source: String
+}
