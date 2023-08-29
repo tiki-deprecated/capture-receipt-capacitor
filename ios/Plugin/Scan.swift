aaa14d518a72936855eb41a967dc3355f3df1a90
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2023-08-29 10:55:21 -0300

    Add Scan Physical

--- ios/Plugin/Scan.swift
+++ ios/Plugin/Scan.swift
@@ -8,12 +8,33 @@ import BlinkReceipt
 import BlinkEReceipt
 import Capacitor
 
-public class Scan {
-    func initialize(req: ReqInitialize){
+public class Scan:  CAPPlugin, BRScanResultsDelegate{
+    public init(req: ReqInitialize){
         BRScanManager.shared().licenseKey = req.licenseKey
         BRScanManager.shared().prodIntelKey = req.productKey
-        BRAccountLinkingManager.shared()
+
     }
+    
+    public func scanPhysical (_ pluginCall: CAPPluginCall){
+        let scanOptions = BRScanOptions()
+        let myViewController = myViewController()
+        let cameraViewController = BRCameraViewController()
+        let rootVc = UIApplication.shared.windows.first?.rootViewController
+        BRScanManager.shared().startStaticCamera(from: rootVc!,
+                                                 cameraType: .uxStandard,
+                                                 scanOptions: scanOptions,
+                                                 with: myViewController)
+        pluginCall.resolve()
+    }
+}
+
+public class myViewController : BRCameraViewController, BRScanResultsDelegate {
+    
+    public func didFinishScanning(_ cameraViewController: UIViewController!, with scanResults: BRScanResults!) {
+        print("###################")
+        print(scanResults!)
+    }
+    
 }
 
 
