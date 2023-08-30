b602a22c3554829da872d694a4687e855c867a07
Miro Benicio <mirodantas27@gmail.com>
2023-08-30 09:49:21 -0300

    WIP: documentation and fixing the typescript methods

--- src/receipt-capture-plugin.ts
+++ src/receipt-capture-plugin.ts
@@ -21,13 +21,43 @@ export interface ReqInitialize{
 }
 
 export interface ReceiptCapturePlugin {
+  /**
+   * Initializes the receipt capture plugin with a license key and product intelligence key.
+   * @param licenseKey - The license key for your application.
+   * @param productKey - The optional product intelligence key for your application.
+   * @throws Error if the initialization fails.
+   */
   initialize(options: ReqInitialize): Promise<void>;
 
+   /**
+   * Login into a retailer or email account to scan for receipts.
+   * @param username - the username of the account.
+   * @param password - the password of the account
+   * @param source - the source from that account, that can be an email service or a retailer service.
+   * @returns - the Account interface with the logged in information.
+   */
   login(options: ReqAccount): Promise<Account>
 
+   /**
+   * Log out from one or all {@link Account}.
+   * @param username - the username of the account that will be logged out.
+   * @param password - the password of the account that will be logged out
+   * @param source - the source from that account, that can be an email service or a retailer service.
+   * @returns - the Account that logged out
+   */
   logout(options?: ReqAccount): Promise<Account>
 
+   /**
+   * Scan for receipts. That can be a physical one, the receipts from an email/retailer account, or all receipts.
+   * @param scanType - The type of the scan.
+   * @param account - The account that will be scanned for receipts.
+   * @returns - The scanned Receipt and a boolean indicates the execution.
+   */
   scan(_option: {scanType: ScanType | undefined, account?: Account}): Promise<{receipt: Receipt, isRunning: boolean}>;
 
+  /**
+   * Retrieves all saved accounts.
+   * @returns - an array of Accounts.
+   */
   accounts(): Promise<Account[]>;
 }
--- src/receipt-capture.ts
+++ src/receipt-capture.ts
@@ -23,7 +23,7 @@ export class ReceiptCapture {
   }
 
   /**
-   * Initializes the receipt capture plugin with a license key and optional product intelligence key.
+   * Initializes the receipt capture plugin with a license key and product intelligence key.
    * @param licenseKey - The license key for your application.
    * @param productKey - The optional product intelligence key for your application.
    * @throws Error if the initialization fails.
@@ -39,34 +39,34 @@ export class ReceiptCapture {
   };
 
   /**
-   * 
-   * @param username 
-   * @param password 
-   * @param source 
-   * @returns 
+   * Login into a retailer or email account to scan for receipts.
+   * @param username - the username of the account.
+   * @param password - the password of the account
+   * @param source - the source from that account, that can be an email service or a retailer service.
+   * @returns - the Account interface with the logged in information.
    */
   login = (username: string, password: string, source: string): Promise<Account> => this.plugin.login({username, password, source})
 
   /**
-   * 
-   * @param username 
-   * @param password 
-   * @param source 
-   * @returns 
+   * Log out from one or all {@link Account}.
+   * @param username - the username of the account that will be logged out.
+   * @param password - the password of the account that will be logged out
+   * @param source - the source from that account, that can be an email service or a retailer service.
+   * @returns - the Account that logged out
    */
   logout = (username: string, password: string, source: string): Promise<Account> => this.plugin.logout({username, password, source})
 
   /**
-   * 
-   * @param scanType 
-   * @param account 
-   * @returns 
+   * Scan for receipts. That can be a physical one, the receipts from an email/retailer account, or all receipts.
+   * @param scanType - The type of the scan.
+   * @param account - The account that will be scanned for receipts.
+   * @returns - The scanned Receipt and a boolean indicates the execution.
    */
   scan = (scanType: ScanType | undefined, account?: Account): Promise<{receipt: Receipt, isRunning: boolean}> => this.plugin.scan({scanType, account})
 
   /**
-   * 
-   * @returns 
+   * Retrieves all saved accounts.
+   * @returns - an array of Accounts.
    */
   accounts = async (): Promise<Account[]> => await this.plugin.accounts()
 }
