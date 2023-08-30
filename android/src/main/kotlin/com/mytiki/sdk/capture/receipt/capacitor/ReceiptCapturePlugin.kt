a8ce06241ee67f6a5d701278a0c970eb77ff1772
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-08-30 09:48:53 -0300

    fix(android): WIP code review

--- android/src/main/kotlin/com/mytiki/sdk/capture/receipt/capacitor/ReceiptCapturePlugin.kt
+++ android/src/main/kotlin/com/mytiki/sdk/capture/receipt/capacitor/ReceiptCapturePlugin.kt
@@ -7,9 +7,7 @@ package com.mytiki.sdk.capture.receipt.capacitor
 
 import android.Manifest
 import android.content.Intent
-import android.os.Build
 import androidx.activity.result.ActivityResult
-import androidx.annotation.RequiresApi
 import com.getcapacitor.PermissionState
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
@@ -35,71 +33,73 @@ import kotlinx.coroutines.async
 class ReceiptCapturePlugin : Plugin() {
     private val receiptCapture = ReceiptCapture()
 
-//    REFACTORED CODE
     @PluginMethod
     fun initialize(call: PluginCall) = receiptCapture.initialize(call, context)
 
     @PluginMethod
-    fun login(call: PluginCall){
-        if(
-            call.data.getString("source").isNullOrEmpty() && !call.data.getString("username").isNullOrEmpty() && !call.data.getString("password").isNullOrEmpty()
-        ) {
+    fun login(call: PluginCall) {
+        val source = call.data.getString("source")
+        val username = call.data.getString("username")
+        val password = call.data.getString("password")
+        if (source.isNullOrEmpty()) {
+            call.reject("Provide source in login request")
+        } else if (username.isNullOrEmpty()) {
+            call.reject("Provide username in login request")
+        } else if (password.isNullOrEmpty()) {
+            call.reject("Provide password in login request")
+        } else {
             val account = Account.fromReq(call.data)
             when (account.accountCommon.type) {
                 AccountTypeEnum.EMAIL -> receiptCapture.email.login(call, account, activity)
                 AccountTypeEnum.RETAILER -> receiptCapture.retailer.login(call, account, context)
             }
-        } else {
-            if(call.data.getString("source").isNullOrEmpty()){
-                call.reject("Provide source in login request")
-            }
-            if(call.data.getString("username").isNullOrEmpty()){
-                call.reject("Provide username in login request")
-            }
-            if(call.data.getString("password").isNullOrEmpty()){
-                call.reject("Provide password in login request")
-            }
         }
-
     }
 
     @PluginMethod
     fun logout(call: PluginCall){
-        if(call.data.getString("source").isNullOrEmpty() && call.data.getString("username").isNullOrEmpty()){
+        val source = call.data.getString("source")
+        val username = call.data.getString("username")
+        val password = call.data.getString("password")
+        if(source.isNullOrEmpty() && username.isNullOrEmpty()){
             receiptCapture.retailer.flush(call)
             receiptCapture.email.flush(call)
-        } else if(!call.data.getString("source").isNullOrEmpty() && !call.data.getString("username").isNullOrEmpty()){
+        } else if(!source.isNullOrEmpty() && !username.isNullOrEmpty()){
             val account = Account.fromReq(call.data)
             when (account.accountCommon.type) {
                 AccountTypeEnum.EMAIL -> {
-                    receiptCapture.email.remove(call, account)
+                    if(password.isNullOrEmpty()){
+                        call.reject("Password is required for email logout.")
+                    }else {
+                        receiptCapture.email.remove(call, account)
+                    }
                 }
                 AccountTypeEnum.RETAILER -> {
                     receiptCapture.retailer.remove(call, account.accountCommon)
                 }
             }
-        } else if(call.data.getString("source").isNullOrEmpty() && !call.data.getString("username").isNullOrEmpty()){
-            call.reject("Provide source in logout request")
-        } else if(!call.data.getString("source").isNullOrEmpty() && call.data.getString("username").isNullOrEmpty()){
-            val accountCommon = AccountCommon.fromString(call.data.getString("source")?: "")
+        } else if(source.isNullOrEmpty() && !username.isNullOrEmpty()){
+            call.reject("Provide source in logout request.")
+        } else if(!source.isNullOrEmpty() && username.isNullOrEmpty()){
+            val accountCommon = AccountCommon.fromString(source)
             if (accountCommon.type == AccountTypeEnum.RETAILER){
                 receiptCapture.retailer.remove(call, accountCommon)
             }else{
-                call.reject("Provide username in email logout request")
+                call.reject("Provide username in email logout request.")
             }
         }
     }
 
     @PluginMethod
     fun accounts(call: PluginCall){
-        val list = mutableListOf<Account>()
         MainScope().async {
+            val list = mutableListOf<Account>()
             val emails = receiptCapture.email.accounts().await()
             val retailers = receiptCapture.retailer.accounts().await()
             emails.forEach {list.add(it)}
             retailers.forEach {list.add(it)}
+            call.resolve(Account.toRspList(list))
         }
-        call.resolve(Account.toRspList(list))
     }
 
     @PluginMethod
@@ -117,14 +117,9 @@ class ReceiptCapturePlugin : Plugin() {
             }
         } else {
             when (req.scanType) {
-                ScanTypeEnum.EMAIL -> receiptCapture.email.scrape(call)
-                ScanTypeEnum.RETAILER -> receiptCapture.retailer.orders(call)
+                ScanTypeEnum.EMAIL -> receiptCapture.email.scrape(call, req.account)
+                ScanTypeEnum.RETAILER -> receiptCapture.retailer.orders(call, req.account)
                else -> call.reject("invalid scan type for account")
-            }
-            if(req.account.accountCommon.source === AccountTypeEnum.RETAILER.name) {
-                receiptCapture.retailer.orders(call, req.account)
-            } else {
-                receiptCapture.email.scrape(call, req.account)
             }
         }
     }
