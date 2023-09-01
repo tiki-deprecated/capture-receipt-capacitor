35f77218727a9cf6f76159305a840ee46fbb70aa
Gabriel Schuler Barros <gabrielschuler3@gmail.com>
2023-09-01 10:15:42 -0300

    fix

---
+++ android/src/test/kotlin/com/mytiki/sdk/capture/receipt/capacitor/AccountTest.kt
@@ -0,0 +1,13 @@
+package com.mytiki.sdk.capture.receipt.capacitor
+
+import io.mockk.mockk
+import org.junit.Test
+
+class AccountTest {
+    val emailAccount = Account(AccountCommon.AMAZON,"username", "password", false)
+
+    @Test()
+    fun toRspTest(){
+
+    }
+}
