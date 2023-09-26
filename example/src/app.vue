6fed5c7745cea0bb018d4733bb042afaebb2c032
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-09-26 16:31:26 -0300

    fix: test form

--- example/src/app.vue
+++ example/src/app.vue
@@ -1,25 +1,61 @@
 <script setup lang="ts">
-import { login, accounts, scan, logout, initialize } from './main';
+import { ref } from 'vue';
+import { login, accounts, scanRetailer, logout, initialize, scanEmail } from './main';
+const username = ref<HTMLInputElement>()
+const pass = ref<HTMLInputElement>()
+const source = ref<HTMLSelectElement>()
 </script>
 
 <template>
   <header>
     <img alt="TIKI Icon" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <style>
+      .greetings {
+        display:flex; 
+        flex-direction:column; 
+        justify-content: space-evenly;
+      }
+
+      .greetings input, .greetings button{
+        margin-top: 10px;
+        height: 48px;
+      }
+    </style>
   </header>
 
   <main>
-    <div class="greetings">
+    <div class="greetings" style="">
       <h1>Oh, Hi!</h1>
-      <button @click="login()">Login</button>
-      <button @click="accounts()">Accounts</button>
-      <button @click="scan()">Scan</button>
-      <button @click="logout()">Logout</button>
-      <button @click="initialize()">initialize</button>
+      <div class="form">
+        <input placeholder="username" type="text" ref="username" />
+        <input placeholder="password" type="password" ref="pass" />
+        <select ref="source">
+          <option value="AMAZON">AMAZON</option>
+          <option value="GMAIL">AMAZON</option>
+        </select>
+        <button @click="initialize()">initialize</button>
+        <button @click="login(username?.value ?? '', pass?.value ?? '', source?.value ?? '')">Login</button>
+        <button @click="accounts()">Accounts</button>
+        <button @click="scanEmail()">Scan Email</button>
+        <button @click="scanRetailer()">Scan Retailer</button>
+        <button @click="logout()">Logout</button>
+      </div>
     </div>
   </main>
 </template>
 
 <style scoped>
+.form{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+
+.form input, .form button, .form select{
+  height: 48px;
+  margin-bottom: 1.5em;
+}
+
 header {
   line-height: 1.5;
 }
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,65 +1,56 @@
 import './assets/main.css';
 import { createApp } from 'vue';
 import { ReceiptCapture } from '../../src/receipt-capture';
-import type { ReceiptCapturePlugin, ScanType } from '@/receipt-capture-plugin';
+import type { ReceiptCapturePlugin } from '@/receipt-capture-plugin';
 import { registerPlugin } from '@capacitor/core';
 
 import App from '@/app.vue';
 import type { AccountType } from '@/account';
+import { AccountTypeCommom } from '@mytiki/tiki-receipt-capacitor';
 
 const plugin: ReceiptCapturePlugin = registerPlugin<ReceiptCapturePlugin>('ReceiptCapture', {
   web: () => import('../../src/receipt-capture-web').then((m) => new m.ReceiptCaptureWeb()),
 });
 
 const instance: ReceiptCapture = new ReceiptCapture(plugin);
-let auth = false;
-let source = '';
 
-const username = '';
-const password = '';
-source = 'GMAIL';
-
-const accountType: AccountType = {
-  type: source === 'GMAIL' ? 'EMAIL' : 'RETAILER',
-  name: source === 'GMAIL' ? 'Gmail' : 'Amazon',
-  key: source,
-};
-
-export const login = async () => {
-  console.log('teste login');
-  console.log(source + ': ' + username);
-  console.log(
-    auth
-      ? await instance.login(source).catch((error) => console.log(error))
-      : await instance.login(source, username, password).catch((error) => console.log(error)),
-  );
-};
+export const login = async (username: string, password: string, source: string) => 
+  await instance.login(username, password, source).catch((error) => console.log(error));
+  
 export const accounts = async () => {
   console.log('teste accounts');
   console.log(await instance.accounts());
 };
-export const scan = async () => {
-  console.log('teste scan');
 
-  const dateInit = new Date();
-  const initialTime = dateInit.getMilliseconds();
-
+export const scanEmail = async () => {
+  console.log('teste scan email');
   try {
-    instance.scan(accountType.type).then((scan) => {
-      let dateFinal = new Date();
-      const finalTime = dateFinal.getMilliseconds();
-
+    instance.scan('EMAIL').then((scan) => {
       console.log(scan);
-      console.log((finalTime - initialTime).toString());
     });
   } catch (e) {
+    console.log('scan error')
     console.log(e);
   }
 };
+
+export const scanRetailer = async () => {
+  console.log('teste scan retailer');
+  try {
+    instance.scan('RETAILER').then((scan) => {
+      console.log(scan);
+    });
+  } catch (e) {
+    console.log('scan error')
+    console.log(e);
+  }
+};
+
 export const logout = async () => {
   console.log('teste logout');
   console.log(await instance.logout());
 };
+
 export const initialize = async () => {
   console.log('teste initialize');
   await instance.initialize(
