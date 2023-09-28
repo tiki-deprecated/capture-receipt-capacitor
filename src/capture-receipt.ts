836e5af5d53d250de2174f06f43db3ffb263f03c
Miro Benicio <mirodantas27@gmail.com>
2023-09-28 19:25:16 -0300

    docs of the capture receipt

--- src/capture-receipt.ts
+++ src/capture-receipt.ts
@@ -41,8 +41,9 @@ export class CaptureReceipt {
   }
 
   /**
-   *
-   * @param account
+   * Connects a Retailer or Gmail account
+   * @param account - the credentials of the account and the Account Type, that identifies 
+   * if it is Gmail or Retailer and which Retailer is.
    * @returns {@link Account} with isVerified .
    */
   async login(account: Account): Promise<Account> {
@@ -52,11 +53,24 @@ export class CaptureReceipt {
     return account;
   }
 
+  /**
+   * Logout an account or flush all the accounts connected
+   * @param account - The account to be disconnected, if the parameter its undefined
+   * the method will flush all the accounts connected
+   */
   async logout(account: Account | undefined = undefined): Promise<void> {
     const reqAccount = account != undefined ? new ReqAccount(account) : undefined;
     this.plugin.logout(reqAccount);
   }
 
+
+  /**
+   * Scan an Gmail/Retailer account for receipts
+   * @param onReceipt - A callback that will be fired when the plugin returns a Receipts
+   * @param daysCutOff - The total of past days that the scan will search
+   * @param onComplete - A callback that will be called when the process finish
+   * @param onError - A callback that will be fired when an error happen
+   */
   scan(
     onReceipt: CallbackMgrCall,
     daysCutOff: number | undefined = 7,
@@ -69,7 +83,12 @@ export class CaptureReceipt {
     this.callbackMgr.add(new CallbackDetails(req.requestId, PluginEvent.onError, onError));
     this.plugin.scan(req);
   }
-
+  /**
+   * Gets all the connected accounts
+   * @param onAccount - A callback that will fire when the plugin returns an account.
+   * @param onComplete - A callback called after the search for account is finished.
+   * @param onError - A callback that will be fired when an error occurs.
+   */
   accounts(
     onAccount: CallbackMgrCall,
     onComplete: CallbackMgrCall | undefined = undefined,
