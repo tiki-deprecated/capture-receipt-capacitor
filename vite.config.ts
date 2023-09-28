a76ef990f75a13dd4f19ecd7015588baf0f9fb89
Mike Audi <mike@mytiki.com>
2023-09-28 17:48:54 -0500

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 
-module.exports = defineConfig({
+export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
@@ -14,26 +14,11 @@ module.exports = defineConfig({
       fileName: (format) => `capture-receipt.${format}.js`,
     },
     rollupOptions: {
-      input: 'dist/esm/index.js',
-      external: ['@capacitor/core'],
-      output: [
-        {
-          file: 'dist/plugin.js',
-          format: 'iife',
-          name: 'capacitorReceiptCapture',
-          globals: {
-            '@capacitor/core': 'capacitorExports',
-          },
-          sourcemap: true,
-          inlineDynamicImports: true,
-        },
-        {
-          file: 'dist/plugin.cjs.js',
-          format: 'cjs',
-          sourcemap: true,
-          inlineDynamicImports: true,
-        },
-      ],
+      external: ['@capacitor/core', '@capacitor/ios', '@capacitor/android'],
+      output: {
+        assetFileNames: (assetInfo): string => assetInfo.name!,
+        exports: 'named',
+      },
     },
     emptyOutDir: false,
   },
