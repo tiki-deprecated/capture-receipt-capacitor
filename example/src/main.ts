f57fdd8724cbf7f96f525d7682eb2b4c856463e1
Mike Audi <mike@mytiki.com>
2023-09-28 22:13:11 -0500

    fix: build example app

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,29 +1,28 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
 import './assets/main.css';
+import type { Account } from '@mytiki/tiki-capture-receipt-capacitor';
+import { accountTypes, instance } from '@mytiki/tiki-capture-receipt-capacitor';
 import { createApp } from 'vue';
-import { CaptureReceipt } from '../../src/capture-receipt';
-import { type Account } from '../../src/account'
-import type { CaptureReceiptPlugin } from '../../src/plugin';
-import { registerPlugin } from '@capacitor/core';
-import App from '@/app.vue';
-import { accountTypes } from 'dist/types';
 
-const plugin: CaptureReceiptPlugin = registerPlugin<CaptureReceipt>('CaptureReceipt', {
-  web: () => import('../../src/receipt-capture-web').then((m) => new m.ReceiptCaptureWeb()),
-});
-
-const instance: CaptureReceipt = new CaptureReceipt(plugin);
+import App from './app.vue';
 
 export const login = async (username: string, password: string, source: string) => {
   const account: Account = {
-    username, password, type: accountTypes.from(source)!!
-  }
+    username,
+    password,
+    type: accountTypes.from(source)!,
+  };
   await instance.login(account).catch((error) => console.log(error));
-}
+};
 
-export const accounts = async () => instance.accounts(onAccount => console.log(onAccount));
-export const scan = async () => instance.scan((onAccount => console.log(onAccount)));
-export const logout = async () => instance.logout();
-export const initialize = async () => {
+export const accounts = async (): Promise<void> => instance.accounts((onAccount) => console.log(onAccount));
+export const scan = async (): Promise<void> => instance.scan((onAccount) => console.log(onAccount));
+export const logout = async (): Promise<void> => instance.logout();
+export const initialize = async (): Promise<void> => {
   await instance.initialize(
     'sRwAAAAoY29tLm15dGlraS5zZGsuY2FwdHVyZS5yZWNlaXB0LmNhcGFjaXRvcgY6SQlVDCCrMOCc/jLI1A3BmOhqNvtZLzShMcb3/OLQLiqgWjuHuFiqGfg4fnAiPtRcc5uRJ6bCBRkg8EsKabMQkEsMOuVjvEOejVD497WkMgobMbk/X+bdfhPPGdcAHWn5Vnz86SmGdHX5xs6RgYe5jmJCSLiPmB7cjWmxY5ihkCG12Q==',
     'wSNX3mu+YGc/2I1DDd0NmrYHS6zS1BQt2geMUH7DDowER43JGeJRUErOHVwU2tz6xHDXia8BuvXQI3j37I0uYw==',
