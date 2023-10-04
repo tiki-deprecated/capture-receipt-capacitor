c6dedada5a5641745be9e1bfedd95f82d8f38382
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-10-04 15:37:44 -0300

    fix: merge conflicts

--- ios/Plugin/Plugin/Req/ReqInitialize‎.swift
+++ ios/Plugin/Plugin/Req/ReqInitialize‎.swift
@@ -17,17 +17,28 @@ public class ReqInitialize : Req{
     /// The product key associated with the ReceiptCapture plugin.
     var productKey: String
     
-    
     /// Initializes the ReqInitialize class with data from a CAPPluginCall.
     ///
     /// - Parameter call: The CAPPluginCall object representing the request.
     /// - Throws: An error if required data is missing.
     override init(_ call: CAPPluginCall) throws {
-        if(call.getString("productKey") == nil || call.getString("licenseKey") == nil ){
-            call.reject("Please, provide a valid LicenseKey and ProductKey")
-            throw NSError()
+        if(call.getString("productKey") == nil){
+          call.reject("Please, provide a valid ProductKey")
+          throw NSError(
+              domain: "tiki",
+              code: 400,
+              userInfo:["message": "Provide a Product Intelligence Key for initialization."]
+          )
         }
-        licenseKey = call.getString("licenseKey")!
+        if(call.getString("ios") == nil ){
+          call.reject("Please, provide a valid iOS License key")
+          throw NSError(
+              domain: "tiki",
+              code: 400,
+              userInfo:["message": "Provide an iOS License Key for initialization."]
+          )
+        }
+        licenseKey = call.getString("ios")!
         productKey = call.getString("productKey")!
         try super.init(call)
     }
